0adf58e2b1f368badc2dc3a6964d2a7e1014a4ec
Vadim Taskaev <vadim.taskaev@outlook.com>
2022-02-19 16:04:04 -0800

    a description of the changes

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
 from dash import Dash, dcc, html, Input, Output
-import os
+import altair as alt
+from vega_datasets import data
 
+cars = data.cars()
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-
-server = app.server
+app = Dash(__name__, external_stylesheets=[
+           'https://codepen.io/chriddyp/pen/bWLwgP.css'])
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-                 'LA',
-                 id='dropdown'
-                 ),
-    html.Div(id='display-value')
-])
+    dcc.Input(id='widget-1'),
+    html.Div(id='widget-2'),
+    html.Div('Hello blue dash', style={'color': 'blue', 'fontSize': 44}),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.Br(),
+    html.P('Hi there', id='my-para', style={'background-color': 'red'}),
+    'This is my slider',
+    dcc.Slider(min=0, max=5, value=2, marks={0: '0', 5: '5'}),
+    'This is my dropdown',
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'San Francisco', 'value': 'SF'}],
+        placeholder='Select a city here...'),
+    'This is my dropdown selection',
+    dcc.Dropdown(
+        options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'San Francisco', 'value': 'SF'}],
+        value='SF', multi=True),
+    html.Iframe(
+        id='scatter',
+        style={'border-width': '0', 'width': '100%', 'height': '400px'}),
+    dcc.Dropdown(
+        id='xcol-widget',
+        value='Horsepower',  # REQUIRED to show the plot on the first page load
+        options=[{'label': col, 'value': col} for col in cars.columns])
+], style={'marginTop': 50})
 
 
-@app.callback(Output('display-value', 'children'),
-              [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+@app.callback(
+    Output('scatter', 'srcDoc'),
+    Input('xcol-widget', 'value'))
+def plot_altair(xcol):
+    chart = alt.Chart(cars).mark_point().encode(
+        x=xcol,
+        y='Displacement',
+        tooltip='Horsepower').interactive()
+    return chart.to_html()
 
 
 if __name__ == '__main__':
